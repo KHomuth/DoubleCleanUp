69ab964fa6549fddcb27bab7ec3b57b539ef0b15
THTV <tienhuytonny.van@haw-hamburg.de>
2023-07-15 12:55:23 +0200

    Boss Phase based on HP

--- Assets/Scripts/BossHealth.cs
+++ Assets/Scripts/BossHealth.cs
@@ -5,25 +5,38 @@ using UnityEngine;
 public class BossHealth : MonoBehaviour
 {
     public static float healthBar = 50f;
-    private GameObject laser;
+    //private GameObject laser;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        laser = GameObject.FindGameObjectWithTag("Laser");
+        //laser = GameObject.FindGameObjectWithTag("Laser");
     }
 
     // Update is called once per frame
     void Update()
     {
-        /*if(healthBar <= 50)
+        if(healthBar == 100f)
         {
-            laser.SetActive(true);
+            Debug.Log("full hp, Phase 1");
         }
-        else
+        else if (healthBar <= 75f && healthBar > 50f)
         {
-            laser.SetActive(false);
-        }*/
+            Debug.Log("Phase 2");
+        }
+        else if (healthBar <= 50f && healthBar > 25f)
+        {
+            Debug.Log("Phase 3");
+        }
+        else if (healthBar <= 25f && healthBar > 0f)
+        {
+            Debug.Log("Phase 4");
+        }
+        else if (healthBar == 0)
+        {
+            Debug.Log("Me deaderinoes");
+        }
+
     }
 }
